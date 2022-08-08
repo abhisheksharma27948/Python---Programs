5f1d4a04256aea43392f2e7f935e4134de254ae6
abhisheksharma27948 <92991046+abhisheksharma27948@users.noreply.github.com>
2022-08-08 21:55:10 +0530

    Update insta_message.py

--- Inst messsage/insta_message.py
+++ Inst messsage/insta_message.py
@@ -1,10 +1,13 @@
+# python -m pip install selenium
+# python.exe -m pip install --upgrade pip
+from asyncio import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time, random
 
 # Username and password of your instagram account:
-my_username = 'abhiabhishek96529'
-my_password = 'Abhc'
+my_username = 'username'
+my_password = 'password'
 
 # Instagram username list for DM:
 usernames = ['user1', 'user2', 'user3']
@@ -16,15 +19,18 @@ messages = ['Hey! Pls follow my page', 'Hey, how you doing?', 'Hey']
 between_messages = 2000
 
 #browser = webdriver.Chrome('chromedriver')
-driver = webdriver.Chrome('chromedriver')
+#driver = webdriver.Chrome('chromedriver')
+driver = webdriver.Chrome(executable_path="C:\\Users\Abhishek Sharma\\OneDrive\Desktop\\ICT Sem 3\\Programming with Python\\Project\\Program\\chromedriver")
 #driver = webdriver.Edge(executable_path="C:\\Users\Abhishek Sharma\\OneDrive\Desktop\\ICT Sem 3\\Programming with Python\\Project\\Program\\msedgedriver.exe")
 #driver = webdriver.Firefox(executable_path="C:\\Users\Abhishek Sharma\\OneDrive\Desktop\\ICT Sem 3\\Programming with Python\\Project\\Program\\geckodriver.exe")
+#driver = webdriver.Firefox(executable_path="C:\\Users\Abhishek Sharma\\OneDrive\Desktop\\ICT Sem 3\\Programming with Python\\Project\\Program\\setup-stub.exe")
 
 # Authorization:
 def auth(username, password):
 	try:
 		driver.get('http://instagram.com')
-		time.sleep(random.randrange(2,4))
+		time.sleep(random.randrange(1,2))
+		#sleep(10000)
 
 		input_username = driver.find_element_by_name('username')
 		input_password = driver.find_element_by_name('password')
@@ -37,6 +43,6 @@ def auth(username, password):
 
 	except Exception as err:
 		print(err)
-		driver.quit()
+		#driver.quit()
   
 auth(my_username, my_password)
